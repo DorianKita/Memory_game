b3f6407f95e47146e30d7d429284978722dc0f6d
Dorian Kita <dorian.kita90@gmail.com>
2023-10-06 20:36:47 +0200

    updated clickCard function

--- game.js
+++ game.js
@@ -1,7 +1,39 @@
 const cardsColor = ['red', 'red', 'blue', 'blue', 'yellow', 'yellow', 'violet', 'violet', 'brown', 'brown', 'cadetblue', 'cadetblue', 'green', 'green', 'gray', 'gray', 'lightgreen', 'lightgreen'];
 
 const cards = [...document.querySelectorAll('div')];
+const startTime = new Date().getTime();
 
+let activeCard = '';
+const activeCards = [];
+
+const gamePairs = cards.length/2;
+let gameResult = 0;
+
+//functions
+const clickCard = function() {
+    activeCard = this;
+    activeCard.classList.remove('hidden');
+
+    if(activeCards.length === 0) {
+        activeCards[0] = activeCard;
+        return;
+    } else {
+        cards.forEach(card => card.removeEventListener('click', clickCard));
+        activeCards[1] = activeCard;
+
+        setTimeout(() => {
+            if(activeCards[0].className === activeCards[1].className){
+                activeCards.forEach( card => card.classList.add('off'));
+                gameResult++;
+            } else {
+                activeCards.forEach( card => card.classList.add('hidden'));
+            }
+            activeCard = '';
+            activeCards.length = 0;
+            cards.forEach(card => card.addEventListener('click', clickCard));
+    },1000);
+    }
+};
 
 const init = () => {
     cards.forEach(card => {
@@ -18,8 +50,6 @@ const init = () => {
     },2000);
 };
 
-const clickCard = () => {
-    console.log('click');
-};
+
 
 init();
